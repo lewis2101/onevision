0ae6d9acf2bfadc40bf87ddef1e84f08beb3ec66
Your Name <you@example.com>
2024-04-10 01:26:46 +0600

    added slider route

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view/>
+  <router-view v-slot="{ Component }">
+    <transition name="slide" mode="out-in">
+      <component :is="Component" :key="$route.path"/>
+    </transition>
+  </router-view>
   <error-modal v-model="modal"/>
 </template>
 
@@ -15,4 +19,11 @@ useRouteLocale()
 
 </script>
 <style lang="scss" scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform .5s;
+}
+.slide-enter-from {
+  transform: translateX(100%);
+}
 </style>
