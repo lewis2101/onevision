ab09b7b10acfe4daf923718f3105b4dec43a19da
Your Name <you@example.com>
2024-04-09 22:09:06 +0600

    added test file

---
+++ src/test/test.ts
@@ -0,0 +1,12 @@
+console.log('starting tests...')
+
+setTimeout(() => {
+  console.log('PASSED - 1')
+}, 1000)
+setTimeout(() => {
+  console.log('PASSED - 2')
+}, 2000)
+setTimeout(() => {
+  console.log('PASSED - 3')
+  console.log('FINISH TEST')
+}, 3000)
