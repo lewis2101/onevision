2cbb7f8fb568e6ed61b5a70a4d11712d87af9f51
Your Name <you@example.com>
2024-04-10 00:34:18 +0600

    added camera

--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -2,10 +2,36 @@
   <div :class="$style.container">
       <v-btn :class="$style.btn" @click="modal">alert</v-btn>
       <v-btn :class="$style.btn" @click="modalInput">propt</v-btn>
+    <div :class="$style.container">
+      <h1>Web Camera Example</h1>
+      <video ref="videoElement" autoplay></video>
+      <v-btn @click="startWebcam">Start Webcam</v-btn>
+      <v-btn @click="stopWebcam">Stop Webcam</v-btn>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+
+const videoStream = ref<any>()
+const videoElement = ref<HTMLVideoElement>()
+
+const startWebcam = async () => {
+  try {
+    videoStream.value = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoElement.value.srcObject = videoStream.value;
+  } catch (error) {
+    console.error('Error accessing webcam:', error);
+  }
+}
+
+const stopWebcam = () => {
+  if (videoStream.value) {
+    videoStream.value.getTracks().forEach(track => track.stop());
+    videoElement.value.srcObject = null;
+  }
+}
 
 const modal = () => {
   alert('DEBUG')
